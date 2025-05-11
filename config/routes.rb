e72942abfb64b2e97cdbdc9feb43511aaf669ab7
Jacob Sanders <sanders.jms@gmail.com>
2025-05-11 01:53:28 -0600

    set up events show action and views"

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [ :show ]
-  resources :events, only: [ :index, :new, :show ]
+  resources :events, only: [ :index, :new, :show, :create ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -13,5 +13,5 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "events#index"
+  root to: "events#index"
 end

170f4809b7874c2e410e66fddbe0e7593f475c4b
Jacob Sanders <sanders.jms@gmail.com>
2025-05-11 01:09:03 -0600

    added create and new action for events, created "new" view to create event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,24 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, except: [ :index ]
   def index
     @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+    if @event.save
+      redirect_to @event, notice: "Post was successfully created"
+    else
+      render "new", status: :unprocessable_entity
+    end
+  end
+
+  private
+  def event_params
+    params.expect(event: [ :name, :location, :date ])
   end
 end

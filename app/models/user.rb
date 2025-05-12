9cca6a2f336a94fd24e2141c07051b0224013371
Jacob Sanders <sanders.jms@gmail.com>
2025-05-12 13:19:38 -0600

    added all assocations for users attending many events and events having many attendings. created table and migrated db

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :username, presence: true
+  has_many :event_attendings, foreign_key: :attendee_id
+  has_many :attended_events, through: :event_attendings
   has_many :created_events, foreign_key: :creator_id, class_name: "Event"
 end
---
+++ db/migrate/20250512184545_create_event_attendings.rb
@@ -0,0 +1,10 @@
+class CreateEventAttendings < ActiveRecord::Migration[8.0]
+  def change
+    create_table :event_attendings do |t|
+      t.references :attendee, null: false, foreign_key: { to_table: :users }, index: true
+      t.references :attended_event, null: false,  foreign_key: { to_table: :events }, index: true
+
+      t.timestamps
+    end
+  end
+end

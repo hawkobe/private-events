5a37b724d243bb3018aa67e89fb286ab84bbaae9
Jacob Sanders <sanders.jms@gmail.com>
2025-05-13 23:58:15 -0600

    made scopes for past events and upcoming events

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
   has_many :event_attendings, foreign_key: :attended_event_id
   has_many :attendees, through: :event_attendings, source: :attendee
   belongs_to :creator, class_name: "User"
+
+  scope :past_events, -> { where("date < ?", Time.zone.now) }
+  scope :upcoming_events, -> {  where("date > ?", Time.zone.now) }
 end
